caa0d47e1271f1faf020d56883f0e0163ea423b0
Mohannad Otaibi <mohannad.otaibi@gmail.com>
2020-10-02 01:49:32 +0900

    Update index.js

--- index.js
+++ index.js
@@ -5,25 +5,33 @@ const { twitch_token, discord_token } = require('./assets/config/config.json');
 const client = new Discord.Client();
 // const twitchJs = new TwitchJs({ username, token })
 
+
+function getChannels(Guilds) {
+	const GuilSubChannels = [];
+
+	Guilds.forEach(oneGuild => {
+		client.guilds.fetch(oneGuild.id).then(guild => {
+			const myMembers = guild.channels.cache.map(channels => new Object({ name: channels.name, id: channels.id }));
+			GuilSubChannels.push(myMembers);
+		}).catch(console.error).finally(()=> {});
+	});
+
+	return GuilSubChannels;
+}
+
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
-
 
 	// List All Guilds
 	const Guilds = client.guilds.cache.map(guild => new Object({ name: guild.name, id: guild.id }));
 	console.log(Guilds);
 
 	// List All Channels
-	let GuildMembers = [];
+	const GuildChannels = getChannels(Guilds);
 
-	Guilds.forEach(oneGuild => {
-		client.guilds.fetch(oneGuild.id).then(guild => {
-			const myMembers = guild.channels.cache.map(members => new Object({ name: members.name, id: members.id }));
-			GuildMembers.push(myMembers);
-		}).catch(console.error).finally(()=> {
-			console.log(GuildMembers);
-		});
-	});
+	
+
+	console.log(GuildChannels);
 
 	// List All Members
 });
